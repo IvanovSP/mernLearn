cf3629f6653edb849bb7aed22625f2228531b150
IvanovSP <ivanovsp88@gmail.com>
2018-08-12 10:50:10 +0300

    router added

--- routs/api/users.js
+++ routs/api/users.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const User = require('../../modules/User');
 
 const router = express.Router();
 
 router.get('/test', (req, res) => res.json({ msg: 'Users works' }));
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email });
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const users = require('./routs/api/users');
 const profile = require('./routs/api/profile');
-const posts = require('./routs/api/posts');
+const posts = require('./routs/api/posts' );
 
 const app = express();
 
